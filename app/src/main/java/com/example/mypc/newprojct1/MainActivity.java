6085b62ba26793a45c2bcd223ca892bc6192c538
dansigit <e.dansi.111@gmail.com>
2019-08-06 09:08:48 +0430

    new commit

--- app/src/main/java/com/example/mypc/newprojct1/MainActivity.java
+++ app/src/main/java/com/example/mypc/newprojct1/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
